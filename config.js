ed5010c1321e23fd8bd7c4a49d8abe4209b79108
dfrodriguezhn-spec <dfrodriguezhn@gmail.com>
2026-06-02 04:54:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbw67c25ZHt4eHoQ9tJxFdb0Sk0EwtMwQE4_mybMdoaJBTADFyMtMp648acIcsaiqg0hJA/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbwYTyDce2IIqbpFG7OkbxxoLn7m4yf1MpPCd4XbCBA7fdTtIo-oGSwLW6bQ05FOZndo/exec"
 };
